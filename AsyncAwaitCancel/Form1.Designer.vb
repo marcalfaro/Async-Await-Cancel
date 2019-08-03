a589f961d8c2726d92c6fbacf8003dae5ba0cd1f
Marc <marcalfaro@live.com>
2019-08-03 08:27:44 +0800

    Add project files.

---
+++ AsyncAwaitCancel/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnToggle = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnToggle
+        '
+        Me.btnToggle.Location = New System.Drawing.Point(35, 57)
+        Me.btnToggle.Name = "btnToggle"
+        Me.btnToggle.Size = New System.Drawing.Size(75, 23)
+        Me.btnToggle.TabIndex = 0
+        Me.btnToggle.Text = "Start"
+        Me.btnToggle.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(162, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(455, 148)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnToggle)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnToggle As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ AsyncAwaitCancel/Form1.vb
@@ -0,0 +1,45 @@
+﻿Option Explicit On
+Imports System.Threading
+
+Public Class Form1
+
+    Private myToken As CancellationTokenSource = Nothing
+
+    Private Async Sub btnToggle_Click(sender As Object, e As EventArgs) Handles btnToggle.Click
+
+        Select Case btnToggle.Text
+            Case "Start"
+                btnToggle.Text = "Stop"
+
+                If myToken IsNot Nothing Then myToken = Nothing
+                myToken = New CancellationTokenSource
+
+                Dim result As String = Await WaitAsync(myToken)
+                MsgBox(result)
+
+                btnToggle.Text = "Start"
+
+            Case "Stop"
+                If myToken IsNot Nothing Then myToken.Cancel()
+        End Select
+
+        ' Display the result.
+
+    End Sub
+
+    Private Async Function WaitAsync(ByVal ct As CancellationTokenSource) As Task(Of String)
+
+        For i As Integer = 1 To 20
+            If ct.IsCancellationRequested = True Then
+                Exit For
+            End If
+            BeginInvoke(Sub(ByVal n As Integer)
+                            Label1.Text = n
+                        End Sub, i)
+            Await Task.Delay(1000)
+        Next
+
+        Return "Finished - async"
+    End Function
+
+End Class
